ed94012bb2a373158462d40f0ea59e95cfbc7e87
xielidan <987905457@qq.com>
2017-08-08 23:46:15 +0800

    完成登录页

--- src/App.vue
+++ src/App.vue
@@ -3,29 +3,65 @@
     <router-view></router-view>
     <!-- tab选项卡 -->
     <div class="bottom">
-      <mu-tabs :value="activeTab" @change="handleTabChange">
-        <mu-tab value="tab1" icon="phone" title="RECENTS"/>
-        <mu-tab value="tab2" icon="favorite" title="FAVORITES"/>
-        <mu-tab value="tab3" icon="person_pin" title="NEARBY"/>
-      </mu-tabs>
+       <mu-paper>
+         <mu-bottom-nav :value="bottomNav" @change="handleChange">
+          <mu-bottom-nav-item value="recents" title="主页" icon="restore" to="/"/>
+          <mu-bottom-nav-item value="favorites" title="机器人" icon="favorite" to="/robot" />
+          <mu-bottom-nav-item value="nearby" title="我的" icon="location_on" to="/home" />
+         </mu-bottom-nav>
+       </mu-paper>
     </div>
+    <!-- 聊天组件 -->
+    <chat v-if="isLogin"></chat>
+    <!-- 注册组件 -->
+    <register></register>
+    <!-- 登录组件 -->
+    <login></login>
+    <!-- 提示组件 -->
+    <dialogmodel></dialogmodel>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'app'
-}
+import { mapState } from 'vuex'
+import Chat from './components/Chat.vue'
+import Login from './components/Login.vue'
+import Register from './components/Register.vue'
+import Dialogmodel from './components/Dialogmodel.vue'
+  export default {
+    name: 'app',
+    data () {
+      return {
+        bottomNav: 'recents'
+      }
+    },
+    methods: {
+      handleChange (val) {
+        this.bottomNav = val
+      }
+    },
+    computed: {
+      ...mapState([
+        'isLogin'
+      ])
+    },
+    components: {
+      Chat,
+      Login,
+      Register,
+      Dialogmodel
+    }
+  }
 </script>
 
 <style>
 #app {
   width: 100%;
   min-height: 100%;
-}
-.bottom {
-  position: fixed;
-  bottom: 0;
-  width: 100%;
+  .bottom {
+    position: fixed;
+    bottom: 0;
+    width: 100%;
+  }
 }
 </style>
