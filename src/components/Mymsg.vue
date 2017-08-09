db7cf4459510854003019a736539a7c1b33519e1
xielidan <987905457@qq.com>
2017-08-09 23:37:04 +0800

    个人信息

---
+++ src/components/Mymsg.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class="clear">
+		<div class="item">
+			<div class="name">
+				{{ name }}
+			</div>
+			<img :src="head" alt="" class="head">
+			<div v-if="img">
+				<img :src="img" alt="" class="img">
+			</div>
+			<span v-if="msg">
+				{{ msg }}
+			</span>
+		</div>
+	</div>
+</template>
+
+<script>
+	
+</script>
